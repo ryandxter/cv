ba2d75a816aad83f01e194e4f97a833ab3d9d0ad
Riansyah Rizky Poetra <ryandxter@users.noreply.github.com>
2017-09-20 07:54:33 +0700

    Update progressbar.js

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,5 +1,5 @@
 function progressBar(percent, $element) {
     "use strict";
     var progressBarWidth = percent * $element.width() / 100;
-    $element.find('div').animate({ width:percent+'%' }, 500).html("<span>"+percent+"</span>");
+    $element.find('div').animate({ width:percent+'%' }, 15000).html("<span>"+percent+"</span>");
 }
